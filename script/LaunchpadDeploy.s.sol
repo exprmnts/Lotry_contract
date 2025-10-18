f2eec18f461570a0ca40eef80b8faa1126a96a1e
Arjun C <50370450+4rjunc@users.noreply.github.com>
2025-10-18 17:31:37 +0530

    name changes (#20)

--- script/LaunchpadDeploy.s.sol
+++ script/LaunchpadDeploy.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {TokenLaunchpad} from "../contracts/Launchpad.sol";
+import {LotryLaunch} from "../contracts/LotryLaunch.sol";
 
 contract LaunchpadDeploy is Script {
     function run() external {
         vm.startBroadcast();
 
         // Deploy the launchpad contract with the deployer as initial owner
-        TokenLaunchpad launchpad = new TokenLaunchpad(msg.sender);
+        LotryLaunch launchpad = new LotryLaunch(msg.sender);
         console2.log("TokenLaunchpad deployed at", address(launchpad));
 
         // Launch a token through the launchpad
