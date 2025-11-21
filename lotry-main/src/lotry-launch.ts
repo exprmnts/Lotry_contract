aed1d7cef2600ce34d76271c87b6ef03e151956f
4rjunc <arjunmundamani@gmail.com>
2025-11-21 23:09:05 +0530

    Initialize subgraph

---
+++ lotry-main/src/lotry-launch.ts
@@ -0,0 +1,36 @@
+import {
+  OwnershipTransferred as OwnershipTransferredEvent,
+  TokenCreated as TokenCreatedEvent
+} from "../generated/LotryLaunch/LotryLaunch"
+import { OwnershipTransferred, TokenCreated } from "../generated/schema"
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTokenCreated(event: TokenCreatedEvent): void {
+  let entity = new TokenCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.tokenAddress = event.params.tokenAddress
+  entity.name = event.params.name
+  entity.symbol = event.params.symbol
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
