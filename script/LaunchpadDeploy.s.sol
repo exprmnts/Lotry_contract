ec3a37acf22031f183a43c66b4c98642b8c75e50
AaroneGeorge <aaroneofficial1879@gmail.com>
2025-12-21 15:22:58 +0530

    smol changes

--- script/LaunchpadDeploy.s.sol
+++ script/LaunchpadDeploy.s.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {LotryLaunch} from "../contracts/LotryLaunch.sol";
-import {LotryTicket} from "../contracts/LotryTicket.sol";
 
 contract LaunchpadDeploy is Script {
     function run() external {
