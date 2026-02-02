aec54e30cf5a6aae9a16ac58fb586fe039e0346a
AaroneGeorge <aaroneofficial1879@gmail.com>
2026-02-02 16:48:13 +0530

    lotry staking update, tests updated make commands added env stake ca changed

--- contracts/LotryStaking.sol
+++ contracts/LotryStaking.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.20;
 
 /*
-                            ⠀⠀⠀⠀⠀ ⠀⢀⣤⣿⣶⣄⠀⠀⠀⣀⡀⠀⠀⠀⠀ 
-                            ⠀⠀⣠⣤⣄⡀⣼⣿⣿⣿⣿⠀⣠⣾⣿⣿⡆⠀⠀⠀  
-                            ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣧⣄⡀⠀  
-                            ⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄  
-                            ⠀⠀⣀⣤⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃  
-                            ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣩⡉⠀⠀  
-                            ⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄  
-                            ⠀⠀⠉⣸⣿⣿⣿⣿⠏⢸⡏⣿⣿⣿⣿⣿⣿⣿⣿⡏  
-                            ⠀⠀⠀⢿⣿⣿⡿⠏⠀⢸⣇⢻⣿⣿⣿⣿⠉⠉⠁⠀  
-                            ⠀⠀⠀⠀⠈⠁⠀⠀⠀⠸⣿⡀⠙⠿⠿⠋⠀⠀⠀⠀  
-                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡀⠀⠀⠀⠀⠀⠀⠀  
+                            ⠀⠀⠀⠀⠀ ⠀⢀⣤⣿⣶⣄⠀⠀⠀⣀⡀⠀⠀⠀⠀
+                            ⠀⠀⣠⣤⣄⡀⣼⣿⣿⣿⣿⠀⣠⣾⣿⣿⡆⠀⠀⠀
+                            ⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣧⣄⡀⠀
+                            ⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+                            ⠀⠀⣀⣤⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃
+                            ⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣩⡉⠀⠀
+                            ⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄
+                            ⠀⠀⠉⣸⣿⣿⣿⣿⠏⢸⡏⣿⣿⣿⣿⣿⣿⣿⣿⡏
+                            ⠀⠀⠀⢿⣿⣿⡿⠏⠀⢸⣇⢻⣿⣿⣿⣿⠉⠉⠁⠀
+                            ⠀⠀⠀⠀⠈⠁⠀⠀⠀⠸⣿⡀⠙⠿⠿⠋⠀⠀⠀⠀
+                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡀⠀⠀⠀⠀⠀⠀⠀
 
-                    
+
                 █   █▀█ ▀█▀ █▀█ █▄█   █▀█ █▀█ █▀█ ▀█▀ █▀█ █▀▀ █▀█ █
                 █▄▄ █▄█  █  █▀▄  █    █▀▀ █▀▄ █▄█  █  █▄█ █▄▄ █▄█ █▄▄
 
@@ -22,7 +22,7 @@ pragma solidity ^0.8.20;
 
 // @title Lotry Staking
 // @author @lotrydotfun
-// @notice This contract is for staking a Lotry Token. Pro-rata reward distribution based on stake percentage. Also daily free ticket claim
+// @notice Staking contract for LOTRY tokens
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -35,60 +35,22 @@ contract LotryStaking is Ownable, ReentrancyGuard {
     // ============ State Variables ============
 
     IERC20 public stakeToken;
-    IERC20 public currentRewardToken;
-
-    uint256 public constant UNLOCK_PERIOD = 2 weeks;
-
     uint256 public totalStaked;
-    uint256 public currentDayRewardPool;
-    uint256 public currentDay;
 
-    // for daily claim
-    uint256 public dailyClaimAmount;
-    IERC20 public dailyClaimToken;
-    uint256 public dailyClaimPoolBalance;
-    uint256 public dailyClaimDay; // track which day this claim is for
+    // Track all stakers
+    address[] public stakers;
+    mapping(address => bool) public isStaker;
+    mapping(address => uint256) public stakedAmount;
 
-    struct StakeInfo {
-        uint256 amount;
-        uint256 unstakeInitiatedAt;
-        bool isUnstaking;
-    }
-
-    struct DayReward {
-        IERC20 rewardToken;
-        uint256 rewardAmount;
-        uint256 totalStakedSnapshot;
-        mapping(address => bool) hasClaimed;
-    }
-
-    mapping(address => StakeInfo) public stakes;
-    mapping(uint256 => DayReward) public dayRewards;
-    mapping(address => uint256) public lastClaimedDay; // track last day user claimed
     // ============ Events ============
 
     event StakeTokenSet(address indexed token);
-    event RewardTokenSet(uint256 indexed day, address indexed token, uint256 amount);
     event Staked(address indexed user, uint256 amount);
-    event UnstakeInitiated(address indexed user, uint256 amount, uint256 unlockTime);
-    event Unstaked(address indexed user, uint256 amount);
-    event RewardClaimed(address indexed user, uint256 indexed day, uint256 amount);
 
     // ============ Errors ============
 
     error ZeroAddress();
     error ZeroAmount();
-    error InsufficientStake();
-    error AlreadyUnstaking();
-    error NotUnstaking();
-    error UnlockPeriodNotPassed();
-    error AlreadyClaimed();
-    error NoRewardForDay();
-    error NoStakeAtSnapshot();
-    error TransferFailed();
-    error NoDailyClaimAvailable();
-    error AlreadyClaimedToday();
-    error InsufficientDailyClaimPool();
 
     // ============ Constructor ============
 
@@ -98,39 +60,34 @@ contract LotryStaking is Ownable, ReentrancyGuard {
 
     // ============ Admin Functions ============
 
-    // @notice Set the staking token address
-    // @param _stakeToken Address of the token to be staked
+    // @notice Set the staking token address (LOTRY)
+    // @param _stakeToken Address of the LOTRY token
     function setStakeToken(address _stakeToken) external onlyOwner {
         if (_stakeToken == address(0)) revert ZeroAddress();
         stakeToken = IERC20(_stakeToken);
         emit StakeTokenSet(_stakeToken);
     }
 
-    // @notice Set daily reward token and deposit rewards
-    // @param _rewardToken Address of the reward token
-    // @param _amount Amount of reward tokens to distribute
-    function setDailyReward(address _rewardToken, uint256 _amount) external onlyOwner nonReentrant {
-        if (_rewardToken == address(0)) revert ZeroAddress();
-        if (_amount == 0) revert ZeroAmount();
+    // @notice Get all staked LOTRY amounts from contract (ONLY OWNER)
+    // @return _stakers Array of staker addresses
+    // @return _amounts Array of staked amounts corresponding to each staker
+    // @return _totalStaked Total amount of LOTRY staked in the contract
+    function getAllStakedAmounts()
+        external
+        view
+        onlyOwner
+        returns (address[] memory _stakers, uint256[] memory _amounts, uint256 _totalStaked)
+    {
+        uint256 length = stakers.length;
+        _stakers = new address[](length);
+        _amounts = new uint256[](length);
 
-        // increment day
-        currentDay++;
+        for (uint256 i = 0; i < length; i++) {
+            _stakers[i] = stakers[i];
+            _amounts[i] = stakedAmount[stakers[i]];
+        }
 
-        // set reward token and pool
-        IERC20 rewardToken = IERC20(_rewardToken);
-        currentRewardToken = rewardToken;
-        currentDayRewardPool = _amount;
-
-        // store day snapshot
-        DayReward storage dayReward = dayRewards[currentDay];
-        dayReward.rewardToken = rewardToken;
-        dayReward.rewardAmount = _amount;
-        dayReward.totalStakedSnapshot = totalStaked;
-
-        // transfer reward tokens to contract
-        rewardToken.safeTransferFrom(msg.sender, address(this), _amount);
-
-        emit RewardTokenSet(currentDay, _rewardToken, _amount);
+        _totalStaked = totalStaked;
     }
 
     // ============ User Functions ============
@@ -141,214 +98,34 @@ contract LotryStaking is Ownable, ReentrancyGuard {
         if (address(stakeToken) == address(0)) revert ZeroAddress();
         if (_amount == 0) revert ZeroAmount();
 
-        StakeInfo storage stakeInfo = stakes[msg.sender];
+        // Add to stakers list if first time staking
+        if (!isStaker[msg.sender]) {
+            stakers.push(msg.sender);
+            isStaker[msg.sender] = true;
+        }
 
-        // can't stake if in unstaking period
-        if (stakeInfo.isUnstaking) revert AlreadyUnstaking();
-
-        // update state
-        stakeInfo.amount += _amount;
+        // Update state
+        stakedAmount[msg.sender] += _amount;
         totalStaked += _amount;
 
-        // transfer tokens
+        // Transfer tokens
         stakeToken.safeTransferFrom(msg.sender, address(this), _amount);
 
         emit Staked(msg.sender, _amount);
     }
 
-    // @notice (starts 2-week unlock period)
-    function initiateUnstake() external nonReentrant {
-        StakeInfo storage stakeInfo = stakes[msg.sender];
+    // ============ View Functions ============
 
-        if (stakeInfo.amount == 0) revert InsufficientStake();
-        if (stakeInfo.isUnstaking) revert AlreadyUnstaking();
-
-        // Start unstaking period
-        stakeInfo.isUnstaking = true;
-        stakeInfo.unstakeInitiatedAt = block.timestamp;
-
-        // remove from total staked (no longer eligible for rewards)
-        totalStaked -= stakeInfo.amount;
-
-        uint256 unlockTime = block.timestamp + UNLOCK_PERIOD;
-        emit UnstakeInitiated(msg.sender, stakeInfo.amount, unlockTime);
+    // @notice Get stake amount for a specific user
+    // @param _user Address of the user
+    // @return Amount staked by the user
+    function getStakeAmount(address _user) external view returns (uint256) {
+        return stakedAmount[_user];
     }
 
-    // @notice Unstake tokens after unlock period, this has to be called by staker to claim the tokens
-    function unstake() external nonReentrant {
-        StakeInfo storage stakeInfo = stakes[msg.sender];
-
-        if (!stakeInfo.isUnstaking) revert NotUnstaking();
-        if (block.timestamp < stakeInfo.unstakeInitiatedAt + UNLOCK_PERIOD) {
-            revert UnlockPeriodNotPassed();
-        }
-
-        uint256 amount = stakeInfo.amount;
-
-        // reset stake info
-        delete stakes[msg.sender];
-
-        // transfer tokens back
-        stakeToken.safeTransfer(msg.sender, amount);
-
-        emit Unstaked(msg.sender, amount);
-    }
-
-    // @notice Claim rewards for a specific day
-    // @param _day Day number to claim rewards for
-    function claimReward(uint256 _day) external nonReentrant {
-        if (_day == 0 || _day > currentDay) revert NoRewardForDay();
-
-        DayReward storage dayReward = dayRewards[_day];
-
-        if (dayReward.hasClaimed[msg.sender]) revert AlreadyClaimed();
-        if (dayReward.totalStakedSnapshot == 0) revert NoStakeAtSnapshot();
-
-        StakeInfo storage stakeInfo = stakes[msg.sender];
-
-        if (stakeInfo.amount == 0) revert NoStakeAtSnapshot();
-
-        if (stakeInfo.isUnstaking) revert NoStakeAtSnapshot();
-
-        // calculate pro-rata reward
-        uint256 userReward = (dayReward.rewardAmount * stakeInfo.amount) / dayReward.totalStakedSnapshot;
-
-        // mark as claimed
-        dayReward.hasClaimed[msg.sender] = true;
-
-        // transfer reward
-        dayReward.rewardToken.safeTransfer(msg.sender, userReward);
-
-        emit RewardClaimed(msg.sender, _day, userReward);
-    }
-
-    // @notice Get user's stake info
-    function getStakeInfo(address _user)
-        external
-        view
-        returns (uint256 amount, uint256 unstakeInitiatedAt, bool isUnstaking, uint256 unlockTime)
-    {
-        StakeInfo storage stakeInfo = stakes[_user];
-        amount = stakeInfo.amount;
-        unstakeInitiatedAt = stakeInfo.unstakeInitiatedAt;
-        isUnstaking = stakeInfo.isUnstaking;
-
-        if (isUnstaking) {
-            unlockTime = unstakeInitiatedAt + UNLOCK_PERIOD;
-        }
-    }
-
-    // @notice Calculate pending reward for a user for a specific day
-    function calculateReward(address _user, uint256 _day) external view returns (uint256) {
-        if (_day == 0 || _day > currentDay) return 0;
-
-        DayReward storage dayReward = dayRewards[_day];
-        if (dayReward.totalStakedSnapshot == 0) return 0;
-        if (dayReward.hasClaimed[_user]) return 0;
-
-        StakeInfo storage stakeInfo = stakes[_user];
-        if (stakeInfo.amount == 0) return 0;
-
-        if (stakeInfo.isUnstaking) return 0;
-
-        return (dayReward.rewardAmount * stakeInfo.amount) / dayReward.totalStakedSnapshot;
-    }
-
-    // @notice  Check if user has claimed reward for a day
-    function hasClaimed(address _user, uint256 _day) external view returns (bool) {
-        return dayRewards[_day].hasClaimed[_user];
-    }
-
-    // @notice get staking rewards of a day
-    function getDayRewardInfo(uint256 _day)
-        external
-        view
-        returns (address rewardToken, uint256 rewardAmount, uint256 totalStakedSnapshot)
-    {
-        DayReward storage dayReward = dayRewards[_day];
-        rewardToken = address(dayReward.rewardToken);
-        rewardAmount = dayReward.rewardAmount;
-        totalStakedSnapshot = dayReward.totalStakedSnapshot;
-    }
-
-    // ============ Daily Claim Admin Functions ============
-
-    // @notice Set daily claim amount and deposit tokens
-    // @dev This resets the claim tracking for a new day
-    // @param _amount Amount each address can claim
-
-    function setDailyClaim(uint256 _amount) external onlyOwner nonReentrant {
-        if (_amount == 0) revert ZeroAmount();
-        if (address(currentRewardToken) == address(0)) revert ZeroAddress();
-
-        // increment daily claim day (resets who has claimed)
-        dailyClaimDay++;
-
-        dailyClaimAmount = _amount;
-        dailyClaimToken = currentRewardToken;
-
-        // reset pool balance for new day
-        dailyClaimPoolBalance = 0;
-
-        //emit DailyClaimAmountSet(dailyClaimDay, _amount);
-    }
-
-    // @notice Deposit tokens for daily claims
-    // @param _amount Amount of tokens to deposit
-    function depositDailyClaimTokens(uint256 _amount) external onlyOwner nonReentrant {
-        if (_amount == 0) revert ZeroAmount();
-        if (address(dailyClaimToken) == address(0)) revert ZeroAddress();
-
-        dailyClaimPoolBalance += _amount;
-
-        dailyClaimToken.safeTransferFrom(msg.sender, address(this), _amount);
-
-        // emit DailyClaimTokenDeposited(dailyClaimDay, address(dailyClaimToken), _amount);
-    }
-
-    // ============ Daily Claim User Functions ============
-
-    // @notice Claim daily tokens
-    // @dev Each address can claim once per day from separate daily claim pool
-    function claimDaily() external nonReentrant {
-        if (dailyClaimAmount == 0) revert NoDailyClaimAvailable();
-        if (address(dailyClaimToken) == address(0)) revert ZeroAddress();
-
-        // Check if user already claimed today
-        if (lastClaimedDay[msg.sender] == dailyClaimDay) revert AlreadyClaimedToday();
-
-        // Check SEPARATE pool has enough tokens (not contract balance)
-        if (dailyClaimPoolBalance < dailyClaimAmount) revert InsufficientDailyClaimPool();
-
-        // Mark as claimed
-        lastClaimedDay[msg.sender] = dailyClaimDay;
-
-        // Decrease tracked pool balance
-        dailyClaimPoolBalance -= dailyClaimAmount;
-
-        // Transfer tokens
-        dailyClaimToken.safeTransfer(msg.sender, dailyClaimAmount);
-    }
-
-    // @notice Check if address can claim daily tokens
-    function canClaimDaily(address _user) external view returns (bool) {
-        if (dailyClaimAmount == 0) return false;
-        if (address(dailyClaimToken) == address(0)) return false;
-        if (lastClaimedDay[_user] == dailyClaimDay) return false;
-        if (dailyClaimPoolBalance < dailyClaimAmount) return false;
-
-        return true;
-    }
-
-    // @notice Get daily claim info
-    function getDailyClaimInfo()
-        external
-        view
-        returns (address token, uint256 amount, uint256 day, uint256 availablePool)
-    {
-        token = address(dailyClaimToken);
-        amount = dailyClaimAmount;
-        day = dailyClaimDay;
-        availablePool = dailyClaimPoolBalance;
+    // @notice Get total number of stakers
+    // @return Number of unique stakers
+    function getStakersCount() external view returns (uint256) {
+        return stakers.length;
     }
 }
